a0e6c89356c4eddc55e545f5ccec4151c2d1a103
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-04-25 15:11:53 +0300

    BaseController

--- src/Controllers/BaseController.cs
+++ src/Controllers/BaseController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BackendTeamwork.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     public class BaseController : ControllerBase
     {
 
