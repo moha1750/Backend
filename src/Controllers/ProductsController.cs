e3c1f48a9915e4b7a54ea15192607bf4660ba645
Almuhannad <almuhannad.khalid@gmail.com>
2024-05-01 14:47:35 +0300

    fixed products based on feedback. Merged with db connection

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -22,12 +22,15 @@ namespace BackendTeamwork.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<Product>> FindMany()
         {
             return Ok(_productService.FindMany());
         }
 
         [HttpGet(":productId")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<Product?> FindOne([FromRoute] Guid productId)
         {
             var targetProduct = _productService.FindOne(productId);
@@ -39,6 +42,8 @@ namespace BackendTeamwork.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<Product> CreateOne([FromBody] Product newProduct)
         {
             if (newProduct is not null)
@@ -50,6 +55,8 @@ namespace BackendTeamwork.Controllers
         }
 
         [HttpPut(":productId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult<Product?> UpdateOne([FromRoute] Guid productId, [FromBody] Product updatedProduct)
         {
             var targetProduct = _productService.UpdateOne(productId, updatedProduct);
@@ -62,6 +69,7 @@ namespace BackendTeamwork.Controllers
 
 
         [HttpDelete(":productId")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult DeleteOne([FromRoute] Guid productId)
         {
             _productService.DeleteOne(productId);
