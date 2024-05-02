64ebbe0a3d94cad2779664a31827191642d01426
Mohammed <mohaza1750@gmail.co>
2024-05-02 14:55:16 +0300

    added required to ID

--- src/Entities/Payment.cs
+++ src/Entities/Payment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace BackendTeamwork.Entities
     {
 
         public Guid Id { get; set; }
+        [Required]
         public int Amount { get; set; }
         public string? Method { get; set; }
         public DateTime Date { get; set; }
