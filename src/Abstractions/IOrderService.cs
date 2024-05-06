ade972612b0cbae2b6080fa72de19e9f70540a09
Mohammed <mohaza1750@gmail.co>
2024-05-06 16:00:58 +0300

    fixing shipping

--- src/Abstractions/IOrderService.cs
+++ src/Abstractions/IOrderService.cs
@@ -1,12 +1,11 @@
 using BackendTeamwork.DTOs;
 using BackendTeamwork.Entities;
-using BackendTeamwork.Enums;
 
 namespace BackendTeamwork.Abstractions
 {
     public interface IOrderService
     {
-        public IEnumerable<OrderReadDto> FindMany(Guid userId, SortBy sortBy);
+        public IEnumerable<OrderReadDto> FindMany(Guid userId);
         public Task<OrderReadDto?> FindOne(Guid orderId);
         public Task<OrderReadDto> CreateOne(OrderCreateDto newOrder);
     }
