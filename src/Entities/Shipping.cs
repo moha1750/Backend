62849e2270c97a5ce107a1bcbb9cb6727250c188
Mohammed <mohaza1750@gmail.co>
2024-05-06 11:30:37 +0300

    fixing entity

--- src/Entities/Shipping.cs
+++ src/Entities/Shipping.cs
@@ -14,21 +14,24 @@ namespace BackendTeamwork.Entities
         [Required, StringLength(30)]
         public string Status { get; set; }
 
+        [Required, StringLength(30)]
+        public string TrackingNo { get; set; }
+
+        [Required, StringLength(30)]
+        public string DeliveryMethod { get; set; }
+
         [Required]
         public DateTime Date { get; set; }
 
         [Required, ForeignKey("Address")]
         public Guid AddressId { get; set; }
 
-        [Required, ForeignKey("Order")]
-        public Guid OrderId { get; set; }
 
         [Required, ForeignKey("User")]
         public Guid UserId { get; set; }
 
 
         public Address Address { get; set; }
-        public Order Order { get; set; }
         public User User { get; set; }
 
 
